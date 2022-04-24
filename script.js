6f50a7bd91438aed57f5eae0a275b2451740d1d0
Cameron Chandler <webstormcam@gmail.com>
2022-04-24 07:54:48 -0400

    Working on the js style of buttons

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let bill_value = document.getElementById('input-price')
-
+let games = document.querySelectorAll('.tip-option').forEach(item =>{
+    item.addEventListener('click', event =>{
+    item.style.backgroundColor ='#26C2AE'
+    item.firstElementChild.style.color ='#00474B'
+   
+    })
+    // item.addEventListener('mouseout',event=>{
+    //     item.style.backgroundColor='rgba(24, 23, 23, 0.555)'
+    //     item.firstElementChild.style.color ='white'
+    // })
+})
 let num_peeps = document.getElementById('people-counter')
 
 
