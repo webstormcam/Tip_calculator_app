ab38ea01bb547d34b988f6a8d93d256aac219627
Cameron Chandler <webstormcam@gmail.com>
2022-04-24 07:18:25 -0400

    Gave the buttons values and styled rest button
    Now I gotta figure out how to select and style the right one and get its value

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ let bill_value = document.getElementById('input-price')
 let num_peeps = document.getElementById('people-counter')
 
 
+
 bill_value.onkeyup = function(){
 console.log(this.value)
+
 }
 
 num_peeps.onkeyup = function(){
     console.log(this.value)
 }
+
+
